0e1b0fbf190a5cfb70a1f12662061e75e497841d
Oscarucho2 <oskarmartinez15@gmail.com>
2023-12-02 20:37:41 -0600

    cambios

--- lib/env/details.dart
+++ lib/env/details.dart
@@ -16,17 +16,61 @@ class DetailsView extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Nombre: ${productDetails['nombre']}'),
-            const SizedBox(height: 8),
-            Text('Precio: \$${productDetails['precio']}'),
-            // Agrega más detalles según la estructura de tus datos
-            // Puedes mostrar la descripción, el ID, u otros detalles del producto
-
+            // Imagen del producto
+            Image.network(
+              productDetails['imagen'] ?? '',
+              height: 400, // Ajusta la altura según sea necesario
+              width: double.infinity, // O ajusta el ancho según tus necesidades
+              fit: BoxFit.cover,
+            ),
             const SizedBox(height: 16),
 
-            // Agrega aquí cualquier otra información que desees mostrar
+            // Nombre del producto
+            Text(
+              productDetails['nombre'] ?? '',
+              style: const TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
 
-            // Puedes personalizar y agregar más widgets según tus necesidades
+            // Precio del producto
+            Text(
+              'Precio: \$${productDetails['precio']}',
+              style: const TextStyle(
+                fontSize: 18,
+                color: Color(0xFF5EC401),
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            // Descripción del producto
+            Text(
+              '${productDetails['descripcion']}',
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            // Botón "Agregar a la cesta"
+            ElevatedButton.icon(
+              onPressed: () {
+                // Agrega la lógica para añadir a la cesta
+                // Puedes utilizar un método o función para manejar esto
+              },
+              icon: const Icon(Icons.shopping_cart, color: Colors.white,),
+              label: const Text(
+                            'Añadir a la cesta',
+                            style: TextStyle(color: Colors.white),
+                          ),
+              style: ElevatedButton.styleFrom(
+                primary: const Color(0xFF5EC401),
+              ),
+            ),
           ],
         ),
       ),
--- lib/env/next_categories.dart
+++ lib/env/next_categories.dart
@@ -5,38 +5,37 @@ import 'package:projectstore/env/details.dart';
 class NextCategories extends StatelessWidget {
   final String categoryName;
 
-  const NextCategories({Key? key, required this.categoryName}) : super(key: key);
+  const NextCategories({Key? key, required this.categoryName})
+      : super(key: key);
 
   Future<List<Map<String, dynamic>>> loadProducts() async {
     // Llama al método para obtener los productos de la categoría actual
-    
+
     return FirebaseService().getProductosPorCategoria(changeName(categoryName));
   }
 
-  String changeName(String name)
-  {
+  String changeName(String name) {
     String nameChange = '';
-    switch (name){
+    switch (name) {
       case 'Frutas y Verduras':
         nameChange = 'frutas_verduras';
-      break;
-        case 'Bebidas':
+        break;
+      case 'Bebidas':
         nameChange = 'bebidas';
-      break;
-        case 'Botanas':
+        break;
+      case 'Botanas':
         nameChange = 'botanas';
-      break;
-        case 'Cremería':
+        break;
+      case 'Cremería':
         nameChange = 'cremeria';
-      break;
-        case 'Carnes':
+        break;
+      case 'Carnes':
         nameChange = 'carnes';
-      break;
-       case 'Mascotas':
+        break;
+      case 'Mascotas':
         nameChange = 'mascotas';
-      break;
+        break;
     }
-  
 
     return nameChange;
   }
@@ -48,45 +47,82 @@ class NextCategories extends StatelessWidget {
         title: Text(categoryName),
       ),
       body: FutureBuilder(
-  future: loadProducts(),
-  builder: (context, AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
-    if (snapshot.connectionState == ConnectionState.waiting) {
-      return const Center(child: CircularProgressIndicator());
-    } else if (snapshot.hasError) {
-      return Center(child: Text('Error: ${snapshot.error}'));
-    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-      return const Center(child: Text('No hay productos disponibles'));
-    } else {
-      // Aquí puedes construir tu interfaz de usuario con la lista de productos
-      return ListView.builder(
-        itemCount: snapshot.data!.length,
-        itemBuilder: (context, index) {
-          final product = snapshot.data![index];
-
-          return InkWell(
-            onTap: () {
-              // Al hacer clic en un producto, navega a la vista de detalles
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DetailsView(productDetails: product),
-                ),
-              );
-            },
-            child: Card(
-              child: ListTile(
-                title: Text(product['nombre'] ?? ''),
-                subtitle: Text('Precio: \$${product['precio']}'),
-                // Otros detalles del producto
+        future: loadProducts(),
+        builder: (context, AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No hay productos disponibles'));
+          } else {
+            return GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // Dos elementos por fila
+                crossAxisSpacing: 8.0,
+                mainAxisSpacing: 8.0,
               ),
-            ),
-          );
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final product = snapshot.data![index];
+
+                return InkWell(
+                  onTap: () {
+                    // Al hacer clic en un producto, navega a la vista de detalles
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            DetailsView(productDetails: product),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.network(
+                          product['imagen'] ?? '',
+                          height:
+                              130, // Ajusta la altura de la imagen según sea necesario
+                          width: 150, // O ajusta el ancho según tus necesidades
+                          fit: BoxFit.cover,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(product['nombre'] ?? ''),
+                              const SizedBox(height: 8),
+                              Text('Precio: \$${product['precio']}'),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        ElevatedButton.icon(
+                          onPressed: () {
+                            // Agrega la lógica para añadir a la cesta
+                            // Puedes utilizar un método o función para manejar esto
+                          },
+                          icon: const Icon(Icons.shopping_cart, color: Colors.white,),
+                          label: const Text(
+                            'Añadir a la cesta',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          style: ElevatedButton.styleFrom(
+                            primary: const Color(0xFF5EC401),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          }
         },
-      );
-    }
-  },
-)
+      ),
     );
   }
 }
-
